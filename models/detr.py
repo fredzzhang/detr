36b0517a9fce965796ae9c4337abfa427b03d07b
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-18 14:09:08 +1100

    Revert "Remove the background class"
    This reverts commit 0bceac609361f9ae3056493de17b5ec3f8290d69.

--- models/detr.py
+++ models/detr.py
@@ -272,7 +272,7 @@ class PostProcess(nn.Module):
         assert target_sizes.shape[1] == 2
 
         prob = F.softmax(out_logits, -1)
-        scores, labels = prob[..., 1:].max(-1)
+        scores, labels = prob[..., :-1].max(-1)
 
         # convert to [x0, y0, x1, y1] format
         boxes = box_ops.box_cxcywh_to_xyxy(out_bbox)
