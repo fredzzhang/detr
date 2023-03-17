7a4ff09adf5ca9c1790f4f82829bebecad38e41b
fred@gcr <frederic.zhang@anu.edu.au>
2023-03-17 06:15:34 +0000

    a minor cleanup

--- models/detr.py
+++ models/detr.py
@@ -310,7 +310,7 @@ def build(args):
     # you should pass `num_classes` to be 2 (max_obj_id + 1).
     # For more details on this, check the following discussion
     # https://github.com/facebookresearch/detr/issues/108#issuecomment-650269223
-    num_classes = 80
+    # num_classes = 80
     # if args.dataset_file == "coco_panoptic":
         # for panoptic, we just add a num_classes that is large enough to hold
         # max_obj_id + 1, but the exact value doesn't really matter
@@ -346,7 +346,7 @@ def build(args):
     losses = ['labels', 'boxes', 'cardinality']
     # if args.masks:
     #     losses += ["masks"]
-    criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
+    criterion = SetCriterion(num_classes=80, matcher=matcher, weight_dict=weight_dict,
                              eos_coef=args.eos_coef, losses=losses)
     criterion.to(device)
     postprocessors = {'bbox': PostProcess()}
